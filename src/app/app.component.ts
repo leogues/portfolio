03cac00ece7eca1e11d21872bd47a41aef996447
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-06-11 15:32:13 -0300

    chore: Update Angular router import in app.routes.ts and add Locomotive Scroll dependency
    The code changes in `app.routes.ts` involve updating the import statement for `Routes` from `@angular/router`. The semicolon at the end of the import statement was removed.
    
    In `package.json`, the `locomotive-scroll` dependency was added with version `5.0.0-beta.13`.
    
    Recent user commits:
    - Add image for musichub project
    - Add isDarkImage property to Technology type and update Technology component
    - Update Go backend technology image and styles
    - Update Go backend technology name to "Go" in projects.json
    - Update Go backend technology name and image
    - Update project dependencies and styles
    - Add portfolio projects and technologies data
    - Update project dependencies and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
+import { AfterViewInit, Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { HeaderComponent } from './header/header.component'
 import { HeroComponent } from './hero/hero.component'
 import { AboutComponent } from './about/about.component'
 import { TechnologiesComponent } from './technologies/technologies.component'
 import { ProjectsComponent } from './projects/projects.component'
+import LocomotiveScroll from 'locomotive-scroll'
 
 @Component({
   selector: 'app-root',
@@ -20,6 +21,26 @@ import { ProjectsComponent } from './projects/projects.component'
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio'
+
+  private locomotiveScroll: LocomotiveScroll | undefined
+
+  ngAfterViewInit() {
+    this.locomotiveScroll = new LocomotiveScroll({
+      lenisOptions: {
+        content: document.querySelector(
+          '[data-scroll-container]',
+        ) as HTMLElement,
+        duration: 1.8,
+        smoothWheel: true,
+      },
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.locomotiveScroll) {
+      this.locomotiveScroll.destroy()
+    }
+  }
 }
