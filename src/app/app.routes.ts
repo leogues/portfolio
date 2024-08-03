684815c565ce9c2a19e662b727ebf1bb66bb9ad6
Leonardo Rodrigues <leo_rodrigues1999@hotmail.com>
2024-08-03 11:04:07 -0300

    chore: removed home routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
 
 export const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () =>
-      import('./pages/home/home.routes').then((m) => m.HOME_ROUTES),
-  },
+  { path: '', component: HomeComponent },
+
   {
     path: 'infra',
     loadChildren: () =>
